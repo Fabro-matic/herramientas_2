1e641b08c31f54db5514e89e730c5581ec9e9b42
Fabro-matic <fabromatics@gmail.com>
2026-02-28 12:31:48 -0300

    Un commit por la dudas

--- practice_reportlab.py
+++ practice_reportlab.py
@@ -1,4 +1,4 @@
-from reportlab.pdfgen import canvas
+#from reportlab.pdfgen import canvas
 
 # def hello(c):
 #     c.drawString(100,100,"Hello World")
@@ -7,27 +7,15 @@ from reportlab.pdfgen import canvas
 # c.showPage()
 # c.save()
 
+from reportlab.pdfgen import canvas
+from reportlab.lib import pdfencrypt
+
+enc=pdfencrypt.StandardEncryption("MIC",canPrint=0)
+
 def hello(c):
- from reportlab.lib.units import inch
- # move the origin up and to the left
- c.translate(inch,inch)
- # define a large font
- c.setFont("Helvetica", 14)
- # choose some colors
- c.setStrokeColorRGB(0.2,0.5,0.3)
- c.setFillColorRGB(1,0,1)
- # draw some lines
- c.line(0,0,0,1.7*inch)
- c.line(0,0,1*inch,0)
- # draw a rectangle
- c.rect(0.2*inch,0.2*inch,1*inch,1.5*inch, fill=1)
- # make text go straight up
- c.rotate(90)
- # change color
- c.setFillColorRGB(0,0,0.77)
- # say hello (note after rotate the y coord needs to be negative!)
- c.drawString(0.3*inch, -inch,  "Hello World")
- c = canvas.Canvas("hello.pdf")
- hello(c)
- c.showPage()
- c.save()
+ c.drawString(100,100,"¡METELE QUE SON PASTELES!")
+c = canvas.Canvas("Un lujo.pdf",encrypt=enc)
+hello(c)
+c.showPage()
+c.save()
+
